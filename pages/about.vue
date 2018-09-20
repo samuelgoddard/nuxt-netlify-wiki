aeb9c2c7bb1393bb76fa9724f4a81e4e7b4a29ba
Sam Goddard <hello@samgoddard.co.uk>
2018-09-20 21:34:39 +0100

    init

---
+++ pages/about.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="container">
+    <Header />
+    <h2 class="subtitle">About</h2>
+    <nuxt-link to="/">Home</nuxt-link>
+  </section>
+</template>
+
+<script>
+import Header from '~/components/Header.vue'
+
+export default {
+  components: {
+    Header
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="container">
+    <Header/>
+    <h2 class="subtitle">Home</h2>
+    <nuxt-link to="about">About</nuxt-link>
+  </section>
+</template>
+
+<script>
+import Header from '~/components/Header.vue'
+
+export default {
+  components: {
+    Header
+  }
+}
+</script>
